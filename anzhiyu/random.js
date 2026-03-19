f718781a187606fb0ab664259cc78c4022c24ac6
WSFREEWS <3485536332@qq.com>
2026-03-19 23:21:34 +0800

    Site updated: 2026-03-19 23:21:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/19/我安装的Obsidian插件/","2024/03/19/公众号订阅列表/","2024/03/19/智能体与语言模型基础/"];function toRandomPost(){
+var posts=["2024/03/19/公众号订阅列表/","2024/03/19/我安装的Obsidian插件/","2024/03/19/智能体与语言模型基础/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
